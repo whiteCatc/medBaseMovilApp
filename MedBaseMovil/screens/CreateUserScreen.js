60c04a28d7baa9e8929b839064c0e8a8ad6c6bf8
whiteCatc <davvf1221@gmail.com>
2024-04-16 16:39:10 -0600

    useStateCreateUserFuncionando

--- MedBaseMovil/screens/CreateUserScreen.js
+++ MedBaseMovil/screens/CreateUserScreen.js
@@ -3,19 +3,30 @@ import { useState } from "react";
 import { View, Text, Button, TextInput, ScrollView, StyleSheet } from "react-native";
 
 const CreateUserScreen = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        phone: ''
+    })
+
+    const handleChangeText = (name, value) => {
+        setState({ ...state, [name]: value })
+    }
+
     return (
         <ScrollView style={styles.container}>
             <View style={styles.inputGroup}>
-                <TextInput placeholder="Nombre" />
+                <TextInput placeholder="Nombre" onChangeText={(value) => handleChangeText('name', value)} />
             </View>
             <View style={styles.inputGroup}>
-                <TextInput placeholder="Email" />
+                <TextInput placeholder="Email" onChangeText={(value) => handleChangeText('email', value)} />
             </View>
             <View style={styles.inputGroup}>
-                <TextInput placeholder="Telefono" />
+                <TextInput placeholder="Telefono" onChangeText={(value) => handleChangeText('phone', value)} />
             </View>
             <View>
-                <Button title="Registrar" />
+                <Button title="Registrar" onPress={() => console.log(state)} />
             </View>
         </ScrollView>
     )

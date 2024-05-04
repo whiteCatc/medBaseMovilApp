fd4f05015968f17ebec5bb9030fe9fb6196fe686
YosefDelRe <118585095+YosefDelRe@users.noreply.github.com>
2024-05-03 19:02:39 -0600

    Add files via upload
    Login base

---
+++ MedBaseMovil/screens/LoginScreen.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { View, Text, Button, TextInput, ScrollView, StyleSheet } from "react-native";
+import { auth } from "../database/firebase";
+
+const LoginScreen = () => {
+
+    const [state, setState] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChangeText = (name, value) => {
+        setState({ ...state, [name]: value })
+    }
+
+    const loginUser = async () => {
+        if (state.email === '' || state.password === '') {
+            alert('Ingresa todos los datos')
+        } else {
+            try {
+                console.log(auth)
+                await auth.signInWithEmailAndPassword(state.email, state.password);
+            } catch (error) {
+                alert('Error al iniciar sesión: ' + error.message)
+            }
+        }
+    }
+
+    return (
+        <ScrollView style={styles.container}>
+            <View style={styles.inputGroup}>
+                <TextInput placeholder="Email" onChangeText={(value) => handleChangeText('email', value)} />
+            </View>
+            <View style={styles.inputGroup}>
+                <TextInput placeholder="Contraseña" secureTextEntry={true} onChangeText={(value) => handleChangeText('password', value)} />
+            </View>
+            <View>
+                <Button title="Iniciar Sesión" onPress={() => loginUser()} />
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 30,
+        flex: 1
+    },
+    inputGroup: {
+        flex: 1,
+        padding: 0,
+        marginBottom: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    }
+})
+
+export default LoginScreen;

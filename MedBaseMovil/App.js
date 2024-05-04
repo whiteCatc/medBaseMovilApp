cb42a146c3bfac81b7869b6dd42c261efee15255
YosefDelRe <118585095+YosefDelRe@users.noreply.github.com>
2024-05-03 19:33:47 -0600

    Screen login

--- MedBaseMovil/App.js
+++ MedBaseMovil/App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { initializeApp } from "@firebase/app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CreateUserScreens from './screens/CreateUserScreen'
 import UserDetailScreen from './screens/UserDetailsScreen'
 import UserList from './screens/UsersList'
-
-
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator()
 
 function MyStack() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name='LoginScreen' component={LoginScreen} />
       <Stack.Screen name='CreateUserScreens' component={CreateUserScreens} />
       <Stack.Screen name='UserDetailsScreen' component={UserDetailScreen} />
       <Stack.Screen name='UserList' component={UserList} />
@@ -37,3 +36,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
